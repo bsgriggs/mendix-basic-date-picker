8d443dbe72dab9c30afbb5a0958a146dd64b6460
bsgriggs <benjaminsgriggs@gmail.com>
2023-07-10 12:44:24 -0400

    init

---
+++ src/BasicDatePicker.editorPreview.tsx
@@ -0,0 +1,23 @@
+import { ReactElement, createElement } from "react";
+import { BasicDatePickerPreviewProps } from "../typings/BasicDatePickerProps";
+import DatePicker from "./components/DatePicker";
+
+export function preview({ class: className, dateFormat }: BasicDatePickerPreviewProps): ReactElement {
+    return (
+        <DatePicker
+            id={"0"}
+            className={className}
+            dateFormat={dateFormat}
+            date={new Date()}
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            setDate={() => {}}
+            readOnly={false}
+            min={undefined}
+            max={undefined}
+        />
+    );
+}
+
+export function getPreviewCss(): string {
+    return require("./ui/BasicDatePicker.css");
+}
